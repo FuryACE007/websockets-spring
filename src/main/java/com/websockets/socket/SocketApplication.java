4301b3c1d8d86da61c7f6e7af1d163e05168b224
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-04-14 11:54:39 +0530

    Finish

--- src/main/java/com/websockets/socket/SocketApplication.java
+++ src/main/java/com/websockets/socket/SocketApplication.java
@@ -3,11 +3,14 @@ package com.websockets.socket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.net.InetSocketAddress;
+
 @SpringBootApplication
 public class SocketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SocketApplication.class, args);
+		SocketServer socketServer = new SocketServer(new InetSocketAddress("0.0.0.0", 8443));
+		socketServer.start();
 	}
 
 }
