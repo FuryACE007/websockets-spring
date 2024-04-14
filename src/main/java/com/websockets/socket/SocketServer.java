e1f02de9cd6cacaa84aa11bfbab1cd1403c2941f
FuryACE007 <sudhanshuvshekhar@gmail.com>
2024-04-14 11:30:14 +0530

    added SocketServer configs

---
+++ src/main/java/com/websockets/socket/SocketServer.java
@@ -0,0 +1,38 @@
+package com.websockets.socket;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+import java.net.InetSocketAddress;
+
+public class SocketServer extends WebSocketServer {
+    public SocketServer(InetSocketAddress address) {
+        super(address);
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket, ClientHandshake clientHandshake) {
+        System.out.println("New Connection");
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println("Closed Connection");
+    }
+
+    @Override
+    public void onMessage(WebSocket webSocket, String s) {
+        System.out.println(s);
+    }
+
+    @Override
+    public void onError(WebSocket webSocket, Exception e) {
+        System.out.println(e.getMessage());
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("WebSocket Server started");
+    }
+}
